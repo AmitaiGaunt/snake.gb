4ec1b256c5d84b05e3ad199255fe06e104f6feac
AmitaiGaunt <git@amitai.ga>
2019-01-20 18:59:41 +0000

    refactoring

--- snake.c
+++ snake.c
@@ -8,99 +8,42 @@
 #define BOOL UINT8
 
 #define MAX_SPRITES 40
+#define WIDTH 10
+#define HEIGHT 18
 #define LENGTH_INIT 3
-#define UP 2
-#define DOWN 3
-#define LEFT 0
-#define RIGHT 1
+#define X 0
+#define Y 1
 #define APPLE 0
+#define UP 0
+#define LEFT 1
+#define RIGHT 2
+#define DOWN 3
 
 INT i;
 INT j;
 INT pos[MAX_SPRITES][2];
-INT front[2];
 INT head;
 INT tail;
 INT direction;
-INT newDirection;
+INT new_direction;
 INT length;
 BOOL loss;
 
-#define COLLIDE(a, b) (pos[a][0] == pos[b][0] && pos[a][1] == pos[b][1])
+#define P(button) (joypad() & button)
+
+#define GET_INPUT {new_direction = P(J_UP) ? UP \
+		: P(J_LEFT) ? LEFT \
+		: P(J_RIGHT) ? RIGHT \
+		: P(J_DOWN) ? DOWN \
+		: new_direction;}
 
 #define MOVE_OBJECT(id, x, y) {\
-		move_sprite(id, x, y); \
-		pos[id][0] = x; \
-		pos[id][1] = y; \
+		pos[id][X] = x; \
+		pos[id][Y] = y; \
+		move_sprite(id, (x + 1) * 8, (y + 2) * 8); \
 	}
 
-#define UPDATE_SWITCHES {\
-		HIDE_WIN;\
-		SHOW_SPRITES;\
-		SHOW_BKG;\
-	}
-
-void checkInput() {
-
-	newDirection = direction;
-
-	// UP
-	if (joypad() & J_UP && direction != DOWN) {
-
-		newDirection = UP;
-
-	}
-
-	// DOWN
-	if (joypad() & J_DOWN && direction != UP) {
-
-		newDirection = DOWN;
-
-	}
-
-	// LEFT
-	if (joypad() & J_LEFT && direction != RIGHT) {
-
-		newDirection = LEFT;
-
-	}
-
-	// RIGHT
-	if (joypad() & J_RIGHT && direction != LEFT) {
-
-		newDirection = RIGHT;
-
-	}
-
-	// Move the sprite in the first movable sprite list (0)
-	//  the the position of X (player[0]) and y (player[1])
-
-}
-
-#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
-
-void move() {
-
-	switch (direction) {
-	case LEFT:
-		front[0] -= 8;
-		break;
-	case RIGHT:
-		front[0] += 8;
-		break;
-	case UP:
-		front[1] -= 8;
-		break;
-	case DOWN:
-		front[1] += 8;
-		break;
-	}
-
-	head = tail;
-	tail = (tail % length) + 1;
-	MOVE_OBJECT(head, front[0], front[1]);
-
-}
+#define COLLIDE(a, b) (pos[a][X] == pos[b][X] && pos[a][Y] == pos[b][Y])
 
 BOOL collides_with_snake(INT id) {
 	for (i = 1; i <= length; i++) {
@@ -111,26 +54,35 @@ BOOL collides_with_snake(INT id) {
 	return 0;
 }
 
-void make_apple() {
-	i = rand();
-	i = i % 19;
-	i = (i + 2) * 8;
-	j = rand();
-	j = j % 17;
-	j = (j + 2) * 8;
-	MOVE_OBJECT(0, i, j);
-}
+#define MAKE_APPLE do { \
+		i = rand(); \
+		i = i % WIDTH; \
+		j = rand(); \
+		j = j % HEIGHT; \
+		MOVE_OBJECT(APPLE, i, j); \
+	} while (collides_with_snake(APPLE));
 
-void check_apple() {
-	if (COLLIDE(APPLE, head)) {
-		make_apple();
-		length++;
+#define CHECK_APPLE if (COLLIDE(APPLE, head)) { \
+		MAKE_APPLE; \
+		length++; \
 	}
-}
+
+#define DED (collides_with_snake(head) || pos[head][X] >= WIDTH || pos[head][Y] >= HEIGHT)
+
+#define UPDATE_SWITCHES {\
+		HIDE_WIN; \
+		SHOW_SPRITES; \
+		SHOW_BKG; \
+	}
+
 
 void init() {
 
-	DISPLAY_ON;						// Turn on the display
+	DISPLAY_ON;
+
+	for (i = 0; i < HEIGHT; i++) {
+		puts("          |");
+	}
 
 	waitpad(0xFF);
 	waitpadup();
@@ -139,27 +91,44 @@ void init() {
 	// Load the the 'sprites' tiles into sprite memory
 	set_sprite_data(0, 1, sprites);
 
+	set_sprite_tile(APPLE, 0x19);
+	MAKE_APPLE;
 	length = LENGTH_INIT;
+	tail = 2;
+	head = 1;
+	direction = DOWN;
+	MOVE_OBJECT(head, 5, 9)
 
-	// Set the first movable sprite (0) to be the first tile in the sprite memory (0)
-	for (i = 1; i <= length; i++) {
-		set_sprite_tile(i, 0);
+}
+
+void move() {
+
+	if (direction + new_direction != 3) {
+		direction = new_direction;
 	}
 
-	front[0] = 80;
-	front[1] = 72;
-	tail = 1;
-	direction = 0;
-	loss = 0;
+	i = pos[head][X];
+	j = pos[head][Y];
 
-	for (i = 0; i < MAX_SPRITES; ++i) {
-		for (j = 0; j < 2; ++j) {
-			pos[i][j] = 0;
-		}
+	switch (direction) {
+	case UP:
+		j--;
+		break;
+	case LEFT:
+		i--;
+		break;
+	case RIGHT:
+		i++;
+		break;
+	case DOWN:
+		j++;
+		break;
 	}
 
-	set_sprite_tile(0, 0x19);
-	make_apple();
+	head = tail;
+	tail = (tail % length) + 1;
+
+	MOVE_OBJECT(head, i, j);
 
 }
 
@@ -168,20 +137,17 @@ void main() {
 	init();
 
 	while (!loss) {
-		checkInput();				// Check for user input (and act on it)
+		GET_INPUT;
 		if (clock() % 8 == 0) {
-			direction = newDirection;
 			move();
-			if (collides_with_snake(head)) {
+			if (DED) {
 				puts("ded");
 				return;
 			}
-			check_apple();
+			CHECK_APPLE;
 		}
-		UPDATE_SWITCHES;			// Make sure the SHOW_SPRITES and SHOW_BKG switches are on each loop
-		wait_vbl_done();			// Wait until VBLANK to avoid corrupting memory
-
-
+		UPDATE_SWITCHES;
+		wait_vbl_done();
 	}
 
 }
